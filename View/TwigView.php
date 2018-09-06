35d75bc63dbe09c89b3f8f9707ccd7d8bd97326e
bluder <victorcpla@gmail.com>
2018-09-06 16:55:52 +0300

    add some functions to Twig

--- View/TwigView.php
+++ View/TwigView.php
@@ -4,6 +4,7 @@ namespace Framework\View;
 
 use Twig_Environment;
 use Twig_Loader_Filesystem;
+use Twig_Function;
 use Framework\View\View;
 
 class TwigView implements View {
@@ -20,6 +21,24 @@ class TwigView implements View {
   public function __construct($path = '') {
     $loader = new Twig_Loader_Filesystem($path);
     $this->engine = new Twig_Environment($loader);
+
+    $this->registerFunctions();
+    $this->registerGlobals();
+  }
+
+  private function registerFunctions() {
+    $wp_head = new Twig_Function('wp_head', 'wp_head');
+    $wp_footer = new Twig_Function('wp_footer', 'wp_footer');
+
+    $this->engine->addFunction($wp_head);
+    $this->engine->addFunction($wp_footer);
+  }
+
+  private function registerGlobals() {
+    $classes = get_body_class();
+    $classesString = implode(' ', $classes);
+
+    $this->engine->addGlobal('body_classes', $classesString);
   }
 
   public function render($template, $data = []) {
